1a82b809ad3d759cbc1e757f024a7fd97c33e3dd
mohamed <mohamed@gmail.com>
2024-12-09 17:31:03 +0100

    i addedd all the functions logics

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     
-    static boolean ishere;
+    public  static boolean ishere;
 
     public static void main(String[] args) {
-        
-                 ishere=true;
+
         do{
 
                     MainMenu();
 
         }
-        while (ishere);
+        while (!ishere);
 
     }
 
     public static void MainMenu(){
+        Scanner input=new Scanner(System.in);
+        String choix;
+        int selected;
 
         System.out.println("-----------------------Menu ---------------------------\n"
                   +"\n        Enter (1) To calculate The  + Addition          \n"
@@ -26,8 +28,24 @@ public class Main {
                   +"\n        Enter (5) To calculate The   Puissance         \n"
                   +"\n        Enter (6) To calculate The   Racine Carre      \n"
                   +"\n        Enter (7) To calculate The   Division          \n"
-                  +"\n                 Enter (8) To Exit                     \n" );
-        
+                  +"\n                 Enter (8) To Exit  " +
+                "                   \n"
+                  +"\n\n        Enter You Choice Number                    \n" );
+
+             choix=input.nextLine();
+             try {
+                 selected=Integer.getInteger(choix);
+                 switch (selected){
+                     case 1:
+
+                         break;
+
+                 }
+
+             } catch (Exception e) {
+                 throw new RuntimeException(e);
+
+             }
 
     }
 }
---
+++ src/utils/Functions.java
@@ -0,0 +1,31 @@
+package utils;
+
+public class Functions {
+    private int Addition(int a,int b){
+        return a+b;
+
+    }
+    private int Multiplication(int a,int b){
+        return a*b;
+    }
+
+    private int Devided(int a,int b){
+        return a/b;
+    }
+
+    private int subtraction(int a,int b){
+        return a-b;
+    }
+
+    private int Puissane(int a,int b){
+        return (int) Math.pow(a,b);
+    }
+    private int Factorial(int a,int b){
+        return a*(b-1);
+    }
+    private int SquireRoot(int a){
+        return (int)Math.sqrt(a);
+    }
+
+
+}
